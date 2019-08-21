08046207dadcfa3b3e003a8c4d12726171e625bb
Adilson Mendes <adilsonmendesramos@hotmail.com>
2019-08-21 22:58:54 +0100

    modifying the app to take the path to logfile.txt as an input argument

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # LogsAnalyser
 A console application that reads from the logs file and flag any logs based in a pre define condition
+
+## Requirements:
+* CommandLine or Eclipse/STS
+
+## Running the application from the command line using Maven
+1. In the base directory (containing pom.xml)
+* mvn clean install
+* mvn exec:java
+
+#### Running just the application test
+
+1. In the base directory (containing pom.xml)
+* mvn test
+
+#### Running application without tests (in case tests are failing)
+
+1. In the base directory (containing pom.xml)
+* mvn -Dmaven.test.skip=true install
+
+#### Extra instructions
+At the begin the program will required the path of the log file, if you don't want to specify the path
+the application will use the default file in the (log-events/logs.text) whitch contains some logs entries
--- src/main/java/com/adilsonmendes/test/creditSuisse/LogsAnalyser/LogsAnalyser.java
+++ src/main/java/com/adilsonmendes/test/creditSuisse/LogsAnalyser/LogsAnalyser.java
@@ -2,6 +2,8 @@ package com.adilsonmendes.test.creditSuisse.LogsAnalyser;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
+
 import org.json.simple.parser.ParseException;
 import com.adilsonmendes.test.creditSuisse.LogsAnalyser.dbOperation.DatabaseOperation;
 import com.adilsonmendes.test.creditSuisse.LogsAnalyser.service.EventService;
@@ -12,10 +14,17 @@ public class LogsAnalyser
 	private static final String filepath = "log-events/logs.txt";
 
 	public static void main(String[] args) throws SQLException, IOException, ParseException {
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Enter the path of the log file that you wish to process\n>");
+		String path = in.nextLine();
+		if(path.isEmpty())
+			path = filepath;
+		
 		DatabaseOperation con = DatabaseOperation.create();
 		String createTable = Helper.readToString("sql/logEvent.sql");
 		con.createTable(createTable);
 
-		EventService.processLogFile(filepath);
+		EventService.processLogFile(path);
 	}
 }
